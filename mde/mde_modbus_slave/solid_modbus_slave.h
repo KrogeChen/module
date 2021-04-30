b617a73154d5f7a09369319d776e5e451e92f99a
KrogeChen <KrogeChen@qq.com>
2021-04-30 11:02:51 +0800

    add files

---
+++ mde/mde_modbus_slave/solid_modbus_slave.h
@@ -0,0 +1,28 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//mbus link solid
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#ifndef solid_log_message_H
+#define solid_log_message_H
+//------------------------------------------------------------------------------
+//+++++++++++++++++++++++++++++++solid+++++++++++++++++++++++++++++++++++++++++
+#define max_solid    1
+//-----------------------------------------------------------------------------
+static log_oper_def log_oper_solid[max_solid];
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+static void log_solid_cfg(void)
+{
+//-----------------------------------------------------------------------------
+    bsp_phy0_cfg();
+    
+    log_oper_solid[0].pull_look_for_byte_rx = bsp_look_for_byte_rx_phy0;
+    log_oper_solid[0].transfet_bytes_tx_phy = bsp_transfet_bytes_tx_phy0;
+    log_oper_solid[0].pull_complete_tx_data = bsp_pull_complete_tx_phy0;
+    log_oper_solid[0].entry_phy_rx = bsp_entry_phy_rx_phy0;
+    log_oper_solid[0].entry_phy_tx = bsp_entry_phy_tx_phy0;
+
+
+ 
+//-----------------------------------------------------------------------------
+}
+#endif
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
